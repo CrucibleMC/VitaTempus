5afdc201fcf29986a6b07a223b9c2af7df6d5071
Bruno Caroba <bruno_caroba@hotmail.com>
2023-08-17 14:31:44 -0300

    Now you can detect if the player has a NecroTempus and get the current version on player client.

--- src/main/java/io/github/cruciblemc/vitatempus/necrotempus/NecroTempus.java
+++ src/main/java/io/github/cruciblemc/vitatempus/necrotempus/NecroTempus.java
@@ -1,13 +1,17 @@
 package io.github.cruciblemc.vitatempus.necrotempus;
 
+import de.tr7zw.changeme.nbtapi.NBTContainer;
+import lombok.SneakyThrows;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import java.io.ByteArrayInputStream;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class NecroTempus implements PluginMessageListener {
 
@@ -19,13 +23,32 @@ public class NecroTempus implements PluginMessageListener {
 
     private NecroTempus(){}
 
-    private static final HashMap<UUID, String> versions = new HashMap<>();
+    private static final ConcurrentHashMap<UUID, String> versions = new ConcurrentHashMap<>();
 
+    public String getVersion(Player player){
+        return versions.get(player.getUniqueId());
+    }
+
+    public boolean hasNecroTempus(Player player){
+        return getVersion(player) != null;
+    }
+
+    @SneakyThrows
     @Override
     public void onPluginMessageReceived(String channel, Player player, byte[] message) {
-        System.out.println(channel);
-        System.out.println(player.getName());
-        System.out.println(Arrays.toString(message));
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(message);
+
+        short size =    byteBuffer.getShort(1);
+        byte[] data =   Arrays.copyOfRange(message, 3, size);
+        NBTContainer nbtContainer = new NBTContainer(new ByteArrayInputStream(data));
+
+
+        if(nbtContainer.hasTag("version")){
+            String version = nbtContainer.getString("version");
+            versions.put(player.getUniqueId(), version);
+        }
+
     }
 
     public void onInit(Plugin plugin, String channel){
